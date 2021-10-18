ed7fd1d82a66c744d8c904a9b26ff0e75c8e6eb2
Asmei <kamsykula@gmail.com>
2021-10-18 23:00:43 +0200

    Correct style of variable naming

--- bots_battles/game_engine/event_handler.py
+++ bots_battles/game_engine/event_handler.py
@@ -2,17 +2,16 @@ import queue
 
 class EventHandler:
     def __init__(self, game_logic, communication_handler):
-        self.__incomingMessages = queue.Queue()
+        self.__incoming_messages = queue.Queue()
         self.__game_logic = game_logic
         self.__communication_handler = communication_handler
 
     def handleIncomingMessages(self):
-        '''Handles a queue with incoming messages, by passing each message to game logic class and sending output message (if any) to queue with outgoing messages'''
-        while not self.__incomingMessages.empty():
-            for e in self.__game_logic.applyRules(self.__incomingMessages.get()):
-                self.__outgoingMessages.put(e)
+        '''Handles a queue with incoming messages, by passing each message to game logic class'''
+        while not self.__incoming_messages.empty():
+            self.__game_logic.applyRules(self.__incoming_messages.get())
 
-    def sendOutcomingMessages(self):
+    def updateGameState(self):
         '''Get actual state of game from GameLogic class instance and pass it to communication handler'''
         self.__communication_handler.pushMessage(self.self.__game_logic.getState())
 
