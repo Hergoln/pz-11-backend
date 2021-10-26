d86466edb62729c167e115a2adc1f6c96cf4d66a
Hergoln <juliusz.malka@o2.pl>
2021-10-26 16:06:12 +0200

    added procfile and setup for heroku deployment

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from website import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=True, host="0.0.0.0", port=port)
