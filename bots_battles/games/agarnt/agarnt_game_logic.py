fixed numerous bug fixes with game server, added delta calculation in… (#8)
* fixed numerous bug fixes with game server, added delta calculation in clock, commented out lines that crashed websocket server, changed the way player position is updated

* added euclidean distance function, added working food eating by players

bots_battles/games/agarnt/agarnt_game_logic.py
@@ -5,6 +5,7 @@ from uuid import UUID
 from bots_battles.game_engine.game_logic import GameLogic
 from .board import Board
 from .agarnt_player import AgarntPlayer
+from util.math import euclidean_distance
 
 class AgarntGameLogic(GameLogic):
     '''
@@ -18,14 +19,18 @@ class AgarntGameLogic(GameLogic):
     def set_players(self, players: Dict[UUID, AgarntPlayer]):
         self.__players = players
 
-    def process_input(self, player_uuid: str, message: Dict[str, str]):
+    def process_input(self, player_uuid: str, message: Dict[str, str], delta: float):
         '''
         Process player inputs using game rules.
         '''
 
         player = self.__players[player_uuid]
-        player.update_position(message['dir'])
+        player.update_position(message['directions'], delta)
         
-        foods_to_remove = [f for f in self.__board if np.isclose(f, (player.x, player.y), (player.get_radius(), player.get_radius()))]
-        player.eat_food(len(foods_to_remove))
-        self.__board.foods.remove(foods_to_remove)
+        try:
+            foods_to_remove = [f for f in self.__board.foods if euclidean_distance(f[0], player.x, f[1], player.y) <= player.get_radius()] 
+            player.eat_food(len(foods_to_remove))
+            for f in foods_to_remove:
+                self.__board.foods.remove(f)
+        except Exception as e:
+            print("FOOD ERROR: ", repr(e))