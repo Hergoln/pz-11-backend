2e24c21f8f2f77c121ef6b5e580f839b6590b7eb
Hergoln <juliusz.malka@o2.pl>
2021-11-19 11:22:44 +0100

    changed models and added sample endpoints

--- website/__init__.py
+++ website/__init__.py
@@ -14,14 +14,15 @@ def create_app(game_server: GameServer, game_factory: GameFactory) -> Flask:
 
     with app.app_context():
         
-        from .services import basic_bp, game_bp, db
+        from .services import basic_bp, game_bp, db, db_bp
         import website.services as sv
         sv.init_game_server(game_server)
         
         app.register_blueprint(basic_bp)
         app.register_blueprint(game_bp)
-
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///:memory:')
+        
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///memory.db')
         db.init_app(app)
+        app.register_blueprint(db_bp)
 
         return app
--- website/services/db_service.py
+++ website/services/db_service.py
@@ -1,22 +1,48 @@
 from flask.blueprints import Blueprint
 from flask_sqlalchemy import SQLAlchemy
+from typing import Optional
+from flask import render_template, jsonify, Blueprint, current_app
+from datetime import datetime, timedelta
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+class Game(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    start_date = db.Column(db.TIMESTAMP)
+    end_date = db.Column(db.TIMESTAMP)
+    states_id = db.Column(db.BigInteger)
+    type_id = db.Column(db.BigInteger)
 
     def __repr__(self) -> str:
-        return f"User('{self.username}')"
+        return f"Game('{self.id}', '{self.start_date}', '{self.end_date}', '{self.states_id}', '{self.type_id}')"
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(60), nullable=False)
+class Game_States(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    states_id = db.Column(db.JSON)
 
     def __repr__(self) -> str:
-        return f"Post('{self.text}')"
+        return f"Game_States('{self.id}', '{self.states_id}')"
 
-def query_users(app):
-    with app.app_context():
-        print(User.query.all())
+class Game_Type(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    type_name = db.Column(db.String)
+
+    def __repr__(self) -> str:
+        return f"Game_Type('{self.id}', '{self.type_name}')"
+
+db_bp = Blueprint("db", __name__)
+
+@db_bp.route('/add_game/')
+def add_user():
+    with current_app.app_context():
+        start_date = datetime.today()
+        end_date = datetime.today() + timedelta(hours=1)
+        new_game = Game(start_date=start_date, end_date=end_date)
+        db.session.add(new_game)
+        db.session.commit()
+        return new_game.__repr__()
+
+@db_bp.route('/all_games')
+def user_all():
+    with current_app.app_context():
+        return str(db.session.query(Game).all())
