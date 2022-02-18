53b477dd0552cef0b5dd946b45ded5993b806941
Hergoln <juliusz.malka@o2.pl>
2022-02-18 10:14:11 +0100

    restored 5 min empty player session timer and aded .json extension to archived files

--- bots_battles/game_engine/game_archive.py
+++ bots_battles/game_engine/game_archive.py
@@ -13,6 +13,6 @@ class JSONGame():
     def dump_to_archive(self) -> None:
         logging.info("dumping to archive")
         with current_app.app_context():
-          with open(os.path.join(current_app.config['SAVED_STATES'], self.info['game_type'], f"agarnt_{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}"), 'wb') as archive:
+          with open(os.path.join(current_app.config['SAVED_STATES'], self.info['game_type'], f"agarnt_{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.json"), 'wb') as archive:
               archive.write(orjson.dumps({'info': self.info, 'states':self.states}))
         logging.info("game dumped")
